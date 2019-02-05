20998ac3044dfe26e3a688130b23881fd3e343c3
Taylor Williams <taylor@thewilliams.net>
2019-02-05 03:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue){
-  const newDrivers =  = drivers.map(function(driver){
+  const newDrivers = drivers.map(function(driver){
     if(driver.revenue > revenue){
       return driver
     }
