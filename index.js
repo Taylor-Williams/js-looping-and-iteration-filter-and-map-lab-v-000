d3a4cecc094b5026e3741f0ed4068ca075d53d6c
Taylor Williams <taylor@thewilliams.net>
2019-02-05 03:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function driverNamesWithRevenueOver(drivers, revenue){
 function exactMatch(drivers, match){
   return drivers.filter(function(driver){
     for(const key in match) {
-      if(drivers[key] === match[key]){
+      return if(drivers[key] === match[key]){
         return driver
       }
     }
